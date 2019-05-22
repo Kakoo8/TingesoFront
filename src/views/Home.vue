cb24f103b46943147419c240a3b2dfea6ff1a6eb
b-vera <bastian.vera.p@usach.cl>
2019-05-21 21:57:13 -0400

    CHG: se realiza la división de módulos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,14 @@
       <el-header>
           <NavBar/>
       </el-header>
-      <el-main>Principal</el-main>
+      <el-row>
+        <el-col :span="18">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple-light"></div>  
+        </el-col>
+      </el-row>
     </el-container>
   </div>
 </template>
@@ -37,5 +44,22 @@ export default {
       text-align: center;
       height: -webkit-fill-available;
     }
-
+    
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
