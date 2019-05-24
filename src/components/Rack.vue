edf32ee53288c0e32faf3a483bf18799eda12a80
b-vera <bastian.vera.p@usach.cl>
2019-05-24 00:09:21 -0400

    CHG: cambio nombres en el ciclo

--- src/components/Rack.vue
+++ src/components/Rack.vue
@@ -1,7 +1,7 @@
 <template>
     <el-card class="box-card">
         <div v-for="o in 4" :key="o" class="text item">
-            {{'List item ' + o }}
+            {{'prueba' + o }}
         </div>
     </el-card>
 </template>
