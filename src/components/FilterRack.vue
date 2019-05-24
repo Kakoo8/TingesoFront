b2a910cded7f6e1eccd98b068173e6b72dbe95b8
Claudio <claudio.urrutia.b@usach.cl>
2019-05-24 10:55:17 -0400

    CHG:Select width

--- src/components/FilterRack.vue
+++ src/components/FilterRack.vue
@@ -1,41 +1,48 @@
 <template>
+    
     <el-card class="box-card">
-    <el-select  v-model="value" clearable placeholder="Capacidad">
-    <el-option>
+      <div class="block">
 
-    </el-option>
-    </el-select>
-    <div class="block">
-        
-        
-        <el-date-picker
-            v-model="value1"
-            type="date"
-            placeholder="Fecha Inicio">
-        </el-date-picker>
-    </div>
-    <div class="block">
-        
-        <el-date-picker
-            v-model="value2"
-            type="date"
-            placeholder="Fecha Termino">
-        </el-date-picker>
-  </div>
+      <el-select  v-model="value" clearable placeholder="Capacidad" >
+        <el-option>
+
+        </el-option>
+      </el-select>
+      </div>
+      <div class="block">
+          
+          
+          <el-date-picker
+              v-model="value1"
+              type="date"
+              placeholder="Fecha Inicio">
+          </el-date-picker>
+      </div>
+      <div class="block">
+          
+          <el-date-picker
+              v-model="value2"
+              type="date"
+              placeholder="Fecha Termino">
+          </el-date-picker>
+        </div>
         <el-button type="primary" icon="el-icon-search">Buscar</el-button>
 
-        <el-card class="box-card">
-            <div v-for="o in 4" :key="o" class="text item">
-            {{'List item ' + o }}
-            </div>
-        </el-card>
-    </el-card>
+          <el-card class="box-card">
+              <div v-for="o in 4" :key="o" class="text item">
+              {{'List item ' + o }}
+              </div>
+          </el-card>
+      </el-card>
 </template>
 
 <style>
     .el-card {
     min-height: 700px;
     max-height: 700px;
+  }
+  .el-input_inner{
+    width: 220px !important
   }
 </style>
 
