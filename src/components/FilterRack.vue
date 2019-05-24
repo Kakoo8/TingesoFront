8fe34096c7ba8c27276704ab8abe18eb688dd7af
Claudio <claudio.urrutia.b@usach.cl>
2019-05-23 20:26:19 -0400

    ADD: Select para filtros

--- src/components/FilterRack.vue
+++ src/components/FilterRack.vue
@@ -1,5 +1,16 @@
 <template>
     <el-card class="box-card">
+    <el-select  v-model="value" clearable placeholder="Select">
+    <el-option>
+
+    </el-option>
+    </el-select>
+    <el-select v-model="value" clearable placeholder="Select">
+    <el-option>
+
+    </el-option>
+    </el-select>
+        <el-button type="primary" icon="el-icon-search">Buscar</el-button>
         <div v-for="o in 4" :key="o" class="text item">
             {{'List item ' + o }}
         </div>
