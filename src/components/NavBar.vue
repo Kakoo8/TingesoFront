419696aa55e49716af85d93905c4b70f5a874eb5
kakoo8 <claudio.urrutia.b@usach.cl>
2019-05-28 10:04:29 -0400

    Cambio de Reserva por ListadoReserva

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,7 @@
     @select="handleSelect"
   >
     <el-menu-item index="1">
-      <router-link to="/" style="text-decoration:none">Reservas</router-link>
+      <router-link to="/" style="text-decoration:none">Listado Reservas</router-link>
     </el-menu-item>
     <el-menu-item index="2">
       <router-link to="/delete" style="text-decoration:none">Eliminar Reserva</router-link>
