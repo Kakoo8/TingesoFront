c092f565245b23dcf72d092fa701632b8c8a44cf
Claudio <claudio.urrutia.b@usach.cl>
2019-05-28 02:20:04 -0400

    CHG:this

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -97,7 +97,7 @@ export default {
             console.log('submit!')
         },
         agregarReserva() {
-            this.axios
+            axios
                 .post('http://157.230.12.110:8080/api/reservations', {
                     checkinName: this.form.checkin_name,
                     lastName: this.lastName,
