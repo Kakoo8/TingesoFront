4242f8ad00b218d0d6c7d66443af9ad549e62c5e
b-vera <bastian.vera.p@usach.cl>
2019-05-28 04:40:43 -0400

    CHG: se agregan imagenenes al componente del manual

--- src/views/Manual.vue
+++ src/views/Manual.vue
@@ -9,22 +9,21 @@
             </div>
             <div class="acordeon-container">
               <el-collapse v-model="activeName" accordion>
-                <el-collapse-item title="Consistency" name="1">
+                <el-collapse-item title="Rack" name="1">
                   <div>Consistent with real life: in line with the process and logic of real life, and comply with languages and habits that the users are used to;</div>
                   <div>Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc.</div>
+                  <img src="../assets/Rack.png" class="image">
                 </el-collapse-item>
-                <el-collapse-item title="Feedback" name="2">
+                <el-collapse-item title="Filtro del rack" name="2">
                   <div>Operation feedback: enable the users to clearly perceive their operations by style updates and interactive effects;</div>
                   <div>Visual feedback: reflect current state by updating or rearranging elements of the page.</div>
+                  <img src="../assets/FiltroRack.png" class="image">
                 </el-collapse-item>
-                <el-collapse-item title="Efficiency" name="3">
+                <el-collapse-item title="Reservas" name="3">
                   <div>Simplify the process: keep operating process simple and intuitive;</div>
                   <div>Definite and clear: enunciate your intentions clearly so that the users can quickly understand and make decisions;</div>
                   <div>Easy to identify: the interface should be straightforward, which helps the users to identify and frees them from memorizing and recalling.</div>
-                </el-collapse-item>
-                <el-collapse-item title="Controllability" name="4">
-                  <div>Decision making: giving advices about operations is acceptable, but do not make decisions for the users;</div>
-                  <div>Controlled consequences: users should be granted the freedom to operate, including canceling, aborting or terminating current operation.</div>
+                  <img src="../assets/Reserva.png" class="image">
                 </el-collapse-item>
               </el-collapse>
             </div>
@@ -67,5 +66,11 @@ export default {
 .acordeon-container {
     padding-left: 3em;
     padding-right: 3em;
+    padding-top: 2em;
+}
+
+img {
+    max-width: 70%;
+    height: auto;
 }
 </style>
